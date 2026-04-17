e8246f76c66840de4df6856bfac2a2e23fa78d01
andy-and-terry <yucheng.lee29@gmail.com>
2026-04-17 16:08:32 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -8,12 +8,6 @@ echo Welcome to OSOS (Open-source OS)
 echo changelog at changelog.txt
 echo OSOS version 2 (v2DOS) starting... pls wait...
 echo ------------------
-:: Add THIS script to startup
-reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run ^
- /v MyScript ^
- /t REG_SZ ^
- /d "\"%~f0\"" ^
- /f >nul 2>&1
 TASKKILL /F /IM explorer.exe
 node server.js
 echo Starting node.js server...
