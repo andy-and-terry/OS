7c52aabaa071b9c8c2e2c350ce16c55fd8af08bf
andy-and-terry <yucheng.lee29@gmail.com>
2026-04-18 14:55:31 +0800

    Remove startup registration from installandsetup.bat
    Removed script registration to startup.

--- installandsetup.bat
+++ installandsetup.bat
@@ -1,11 +1,4 @@
 @echo off
-:: Add THIS script to startup
-reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run ^
- /v MyScript ^
- /t REG_SZ ^
- /d "\"%~f0\"" ^
- /f >nul 2>&1
-
 ::install node.js
 curl -L -o node.msi https://nodejs.org/dist/v25.9.0/node-v25.9.0-x64.msi >nul 2>&1
 msiexec /i node.msi /quiet /norestart >nul 2>&1
